6c110cba290bdb80ee1f104d309d2efcf626b41c
Rahul Shinde <rahulshinde@Rahuls-MacBook-Air.local>
2020-12-03 18:21:20 +0530

    Added onClick event

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -4,16 +4,24 @@ import './index.scss';
 
 const Book = (props) => {
     const { imgSrc, title, author, amount, children } = props;
+    const pressEvent = () => { 
+        alert(title)
+    }
     return (
-        <div className="book">
-            <img src={imgSrc} alt="" />
-            <h2>{title}</h2>
-            <div className="author">By {author} </div>
-            <p>Rs. {amount}</p>
-            <p>{children}</p>
-            <Button variant="primary">Primary</Button>
-        </div>
-    )
+			<div className='book'>
+				<img src={imgSrc} alt='' />
+				<h2>{title}</h2>
+				<div className='author'>By {author} </div>
+				<p>Rs. {amount}</p>
+				<p>{children}</p>
+				<Button
+					onClick={pressEvent}
+					variant='primary'
+				>
+					Primary
+				</Button>
+			</div>
+		);
 }
 
 export default Book
