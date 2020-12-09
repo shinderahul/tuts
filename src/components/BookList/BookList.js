875a301881602b3a1f54144ca8acb94657fbc5ed
Sameer <samparab79@gmail.com>
2020-12-09 16:37:52 +0530

    unique code added

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Book from '../Book/Book'
 import {data} from './books'
 import './index.css'
+import { v4 as uuidv4 } from 'uuid';
 
 const BookList = () => {
     return (
         <div className="book-list">
             {data.map((book) => { 
-                return <Book {...book} />
+            return <Book {...book} key={uuidv4()}/>
             })}
         </div>
     )
